41415f8563c0a0d59d86ddc2ceb81094a38176b5
Ihor Leonov <ihorleonow@gmail.com>
2023-02-05 17:23:11 +0200

    add get ganres fn

--- src/js/search-movies.js
+++ src/js/search-movies.js
@@ -1,27 +1,27 @@
-import SearchMovieApi from './themoviedb-api-class'; // импортируем класс
+import SearchMovieApi from './themoviedb-api-class';
 import filmCardTemplate from '../templates/card_film.hbs';
 
 const searchForm = document.querySelector('.js-search-form');
-
 const cardBox = document.querySelector('.js-card-collection');
-const searchMovieApi = new SearchMovieApi(); // создаем экземпляр класса
+const searchMovieApi = new SearchMovieApi();
 searchForm.addEventListener('submit', onSearch);
-// getGenres();
+let movieGenres = [];
+getGenres(); // получаем жанры (нужно переделать чтобы хранились в local storage)
 
 async function onSearch(evt) {
   evt.preventDefault();
   try {
     searchMovieApi.resetPage();
-    searchMovieApi.quary = searchForm.query.value.trim(); // для поиска инфо по id также записываем значение id в searchMovieApi.movieId
+    searchMovieApi.quary = searchForm.query.value.trim();
 
     if (!searchMovieApi.quary) {
       console.log('Введите название фильма!');
       resetMarkup();
       return;
     }
-    
+
     const { page, results, total_pages, total_results } =
-      await searchMovieApi.getMovieByName(); // здесь меняем на свой метод класса getPopularFilms()/getInfoByMovieId() и деструктуризируем необходимые данные из респонса
+      await searchMovieApi.getMovieByName();
 
     if (total_results === 0) {
       resetMarkup();
@@ -31,39 +31,69 @@ async function onSearch(evt) {
 
     resetMarkup();
     createCardMarkup(results);
-  } finally {
-    console.log();
+  } catch (error) {
+    console.log(error.message);
   }
 }
-
+// в createCardMarkup(arr) на базе results создаем новый массив newArr,
+// в котором id жанров превращаются в строку жанров, а дату обрезаем,
+// и прокидываем newArr в функцию hbs
 function createCardMarkup(arr) {
   let newArr = [];
   arr.map(({ poster_path, title, release_date, genre_ids }) => {
+    const filmGenres = getGenre(genre_ids).join(', ');
     const filmDate = release_date.slice(0, 4);
-    const obj = { poster_path, title, filmDate, genre_ids };
+    const obj = { poster_path, title, filmDate, filmGenres };
     newArr.push(obj);
   });
-  // const markup = newArr.map(ar => filmCardTemplate(ar)).join('');
+
   const markup = filmCardTemplate(newArr);
   cardBox.insertAdjacentHTML('beforeend', markup);
 }
 
-function resetMarkup() {
-  cardBox.innerHTML = '';
+// getGenres() получает массив обьектов жанров с бекенда и записывает в movieGenres
+async function getGenres() {
+  try {
+    const { genres } = await searchMovieApi.getMovieGenres();
+    movieGenres = genres;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-// async function getGenres() {
+// getGenre(arr) перебирает массив объектов жанров и сравнивает id с id,
+// которые приходят в карточке фильма,
+// если совпадают, то передает название жанра в новый массив,
+// и возвращает этот массив
+function getGenre(arr) {
+  let genrNames = [];
+  movieGenres.map(({ id, name }) => {
+    arr.map(ar => {
+      if (ar === id) {
+        genrNames.push(name);
+      }
+    });
+  });
+  return genrNames;
+}
+
+// async function getGenresTest() {
 //   try {
+//     let obj = { genr: 1 };
+//     let doubleObj = {};
 //     const genres = await searchMovieApi.getMovieGenres();
 //     console.log(genres);
-//     let genId;
-//     let genresObj = { genId: '1' };
 //     genres.genres.map(({ id, name }) => {
-//       genId = id;
-//       genresObj.genId = name;
+//       doubleObj = Object.fromEntries(
+//         Object.entries(obj).map(([key, value]) => [(key = name), (value = id)])
+//       );
 //     });
-//     console.log(genresObj[0]);
+//     console.log(doubleObj);
 //   } catch (error) {
 //     console.log(error.message);
 //   }
 // }
+
+function resetMarkup() {
+  cardBox.innerHTML = '';
+}
