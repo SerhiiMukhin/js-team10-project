58f9d098c48eee991cb513d06fdc5757cb6b3433
Irene5698 <110497689+Irene5698@users.noreply.github.com>
2023-02-05 21:03:37 +0200

    Зміни в адаптації

---
+++ src/js/spiner.js
@@ -0,0 +1,12 @@
+const spiner = document.querySelector('.spiner')
+
+function spinerOff(){
+    spiner.classList.add('hidden')
+}
+
+function spinerOn(){
+    spiner.classList.remove('hidden')
+}
+
+export {spinerOff, spinerOn}
+
---
+++ src/js/themoviedb-api-class.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'a59dfea75e9e40e203f0819879862061';
+
+export default class SearchMovieApi {
+  constructor() {
+    this.searchQuary = '';
+    this.page = 1;
+    this.movieId = '';
+    this.lang = 'en-US';
+  }
+
+  async getMovieByName() {
+    const url = `${BASE_URL}search/movie?api_key=${API_KEY}&query=${this.searchQuary}&page=${this.page}&language=${this.lang}&include_adult=false`;
+    const response = await axios.get(url);
+    return response.data;
+  }
+
+  async getPopularFilms() {
+    const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=${this.lang}`;
+    const response = await axios.get(url);
+    return response.data;
+  }
+
+  async getInfoByMovieId() {
+    const url = `${BASE_URL}movie/${this.movieId}?api_key=${API_KEY}&language=${this.lang}`;
+    const response = await axios.get(url);
+    return response.data;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get quary() {
+    return this.searchQuary;
+  }
+
+  set quary(newQuary) {
+    this.searchQuary = newQuary;
+  }
+}
