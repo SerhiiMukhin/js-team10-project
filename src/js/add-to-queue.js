f5df7cb629fedc69156bb78d2dc43ae5a839dbc1
Pumpon4yk <tt10392@gmail.com>
2023-02-05 10:23:24 +0100

    fix

--- src/js/add-to-queue.js
+++ src/js/add-to-queue.js
@@ -6,13 +6,13 @@
 //для запису одного фільма викликаємо setQueueOne(фільм) він приймає обєкт (цілий обєкт одного фільма) і записує в масив в локальне сховище(LS), 
 //якщо фільм добавило повертає true якщо виникла помилка поверне false.
 
-//для того щоб отримати один збережений фільм викликаємо toQueue.getQueueOne(фільм) приймає цілий обєкт, шукає в LS  по id фільм, повертає масив з одним обєктом, 
+//для того щоб отримати один збережений фільм викликаємо toQueue.getQueueOne(ід) приймає ід фільма, шукає в LS  по id фільм, повертає масив з одним обєктом, 
 //якщо фільм не знайшло повертає пустий масив, якщо не знайшло записів про Json в LS  поверне undefined, якщо виникла помилка з LS повертає false.
  
 //для того щоб повернути всі збережені фільми toQueue.getQueueAll(фільм) повертає масив обєктів
 //якщо фільми не знайшло повертає пустий масив, якщо не знайшло записів про Json в LS  поверне undefined, якщо виникла помилка з LS повертає false.
 
-//для видалення фільму з черги removeQueueOneEl(фільм) передаємо цілий обєкт , перезаписує масив в LS без переданого вільму і повертає true якщо все успішно , 
+//для видалення фільму з черги removeQueueOneEl(ід) передаємо ід фільма , перезаписує масив в LS без переданого вільму і повертає true якщо все успішно , 
 //якщо не знайшло записів про Json в LS  поверне undefined, якщо виникла помилка з LS повертає false.
 
 
@@ -28,8 +28,8 @@ export const  toQueue = {
     },
 
 
-    removeMovie(el, arr){
-        return arr.filter(e => e.id !== el.id)
+    removeMovie(id, arr){
+        return arr.filter(e => e.id !== id)
     },
 
 
@@ -45,12 +45,12 @@ export const  toQueue = {
     },
 
 
-    getQueueOne(el){
+    getQueueOne(id){
         try{
         if (localStorage.getItem(QUEUE)) {
             const arr = JSON.parse(localStorage.getItem(QUEUE));
 
-            return arr.filter(e => e.id === el.id)
+            return arr.filter(e => e.id === id)
         }
         return undefined
     }catch{
@@ -80,12 +80,12 @@ export const  toQueue = {
     },
 
 
-    removeQueueOneEl(el){
+    removeQueueOneEl(id){
         try{
         if (localStorage.getItem(QUEUE)) {
             const arr = JSON.parse(localStorage.getItem(QUEUE));
 
-            localStorage.setItem(QUEUE, JSON.stringify(this.removeMovie(el, arr)))
+            localStorage.setItem(QUEUE, JSON.stringify(this.removeMovie(id, arr)))
             return true
         }
         return undefined
