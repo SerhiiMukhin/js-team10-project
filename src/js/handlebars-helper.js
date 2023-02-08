56c2123cc45324791332c4a3697adf887a3dfa11
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2023-02-08 23:40:17 +0200

    poster fix

---
+++ src/js/handlebars-helper.js
@@ -0,0 +1,9 @@
+import Handlebars from "handlebars";
+
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if (a == b) {
+        return opts.fn(this);
+    } else {
+        return opts.inverse(this);
+    }
+});
