8d1e640470cceb19ef852015e5400eb7a0f4329a
OLEH RUDENKO <51856760+GrazyMen@users.noreply.github.com>
2023-02-09 12:05:10 +0200

    team-modal

--- src/js/teamModal.js
+++ src/js/teamModal.js
@@ -1,6 +1,6 @@
   window.addEventListener('keydown', closeModalByEscape);
   function closeModalByEscape(e) {
-    if (e.code === 'Escape') {
+    if (e.code === '27') {
       modal.close();
       window.removeEventListener('keydown', closeModalByEscape);
     }
